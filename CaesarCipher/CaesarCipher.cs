951685e6bf5a502a360851faf5b2ba0a78f1cf0e
sagar.saren <sagar.saren@cgi.com>
2019-08-29 18:27:59 +0530

    first commit

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CaesarCipher
 {
@@ -7,10 +8,32 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
+            char[] textChar = text.ToCharArray();
+            Queue<char> textQueue = new Queue<char>();
+            foreach (char letter in textChar)
+            {
+            if(((int)letter >=65 && (int)letter<=90) || ((int)letter >=97 && (int)letter<=122) )
+            {
+                if (Char.IsUpper(letter))
+                {
+                    textQueue.Enqueue((char)(((int)letter + shiftKey - 65)%26 + 65));
+                }
+                else
+                {
+                    textQueue.Enqueue((char)(((int)letter + shiftKey - 97)%26 + 97));
+                }
+            }
+                
+            }
+            char[] textCharNew = textQueue.ToArray();
+            string textNew = new string(textCharNew);
+            return textNew;
             throw new NotImplementedException("You need to implement this function.");
         }
         public static void Main(string[] args){
-            
+            string text = Console.ReadLine();
+            int shiftKey = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Rotate(text,shiftKey));
         }
     }
 }
